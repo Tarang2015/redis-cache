801e7d99009f660994460c2eb112c98b430b5a1c
Tarang2015 <tribhuwan.sams@gmail.com>
2020-06-04 10:15:24 +0530

    chnaged

--- src/main/java/com/ibs/redis/rediscache/config/AuthenticationWebFilter.java
+++ src/main/java/com/ibs/redis/rediscache/config/AuthenticationWebFilter.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class AuthenticationWebFilter extends WebSecurityConfigurerAdapter{
 
+  
   @Autowired
   private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
